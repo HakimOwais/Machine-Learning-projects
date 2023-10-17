1724b6b189ccf6440b05e14d78c40cbce36a7fef
HakimOwais <owaisibnmushtaq@gmail.com>
2023-10-17 11:36:00 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    """_summary_
+        This function will return the list of requirements
+    Args:
+        file_path (str): _description_
+
+    Returns:
+        List[str]: _description_
+    """
+    requirements= []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        [req.replace("\n"," ") for req in requirements]
+    
+
+setup(
+    name= "MLProjects",
+    version= "0.0.1",
+    author= "Owais Bin Mushtaq",
+    author_email= "owaisibnmushtaq@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
